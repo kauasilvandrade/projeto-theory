55a172b441d8838b9d15a86ff9a8f878d57ce33f
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-09 20:05:51 -0300

    menu terminado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const btnOpen = document.querySelector('.btn-open');
 const Menu = document.querySelector('.lista-menu');
 
+function mudouTamanho() {
+    if (window.innerWidth <= 768) {
+        btnOpen.classList.remove('ativado');
+        Menu.classList.remove('ativado');
+    }
+}
+
 btnOpen.addEventListener('click', () => {
     btnOpen.classList.toggle('ativado');
     Menu.classList.toggle('ativado');
