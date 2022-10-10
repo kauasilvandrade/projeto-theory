967440789c4d25d64e54eb6fca6dbeb61e9d6420
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-10 08:47:56 -0300

    site finalizado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btnOpen = document.querySelector('.btn-open');
 const Menu = document.querySelector('.lista-menu');
 
+// QUANDO MUDAR O TAMANHO DA TELA, O MENU VAI SUMIR
 function mudouTamanho() {
     if (window.innerWidth <= 768) {
         btnOpen.classList.remove('ativado');
@@ -8,6 +9,7 @@ function mudouTamanho() {
     }
 }
 
+// BOTÃO ABRIR E FECHAR MENU
 btnOpen.addEventListener('click', () => {
     btnOpen.classList.toggle('ativado');
     Menu.classList.toggle('ativado');
