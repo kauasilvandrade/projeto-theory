ee021946d1d58c38d555f521ca5dc4a277cd0ac8
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-11-10 19:49:39 -0300

    organizando o código

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const btnOpen = document.querySelector('.btn-open');
 const Menu = document.querySelector('.lista-menu');
 
@@ -9,8 +10,28 @@ function mudouTamanho() {
     }
 }
 
-// BOTÃO ABRIR E FECHAR MENU
-btnOpen.addEventListener('click', () => {
+//FUNÇÃO DO BOTÃO ABRIR E FECHAR MENU
+function abrirMenu(event) {
+
+    if (event.type === "touchstart") event.preventDefault();
+
     btnOpen.classList.toggle('ativado');
+
     Menu.classList.toggle('ativado');
-})
+
+    const ativado = btnOpen.classList.contains('ativado')
+
+    event.currentTarget.setAttribute('aria-expanded', ativado)
+
+    if (ativado) {
+        event.currentTarget.setAttribute('aria-label', 'Botão fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Botão abrir menu')
+    }
+
+}
+
+btnOpen.addEventListener('click', abrirMenu);
+btnOpen.addEventListener('touchstart', abrirMenu);
+
+// LEMBRAR DE TESTAR O SET ATTRIBUTE
